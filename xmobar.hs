1f8d57e782e8eed572f36d52e02a579ec953b4fc
Yannis Spiliopoulos <yannis@cs.columbia.edu>
2017-02-01 14:13:47 -0500

    Show volume meter

--- xmobar.hs
+++ xmobar.hs
@@ -15,7 +15,7 @@ Config {
         Run MultiCpu ["-t","Cpu: <total0> <total1> <total2> <total3>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
         Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Network "em1" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Com "/bin/bash" ["-c", "/home/giannis/.xmonad/progress.sh $(/home/giannis/.xmonad/volume.sh) 10"] "volume" 1,
         Run Date "%a %b %_d %l:%M" "date" 10,
         Run StdinReader
     ],
