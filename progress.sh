0e2ccc4b5059f139d7a3a4fd2686b61c7b20479c
Yannis Spiliopoulos <yannis@cs.columbia.edu>
2017-02-02 12:48:05 -0500

    Updated progress bar with better formating and color

--- progress.sh
+++ progress.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-  echo "$0 percentage length"
+if [ "$#" -ne 3 ]; then
+  echo "$0 percentage length decimals"
   exit 1
 fi
 
 percentage=$1
 length=$2
+decimals=$3
 
 if [[ $(echo "$percentage > 100" | bc) = 1 || $(echo "$percentage < 0" | bc) = 1 ]]
 then
@@ -15,23 +16,31 @@ then
 fi
 
 echo -n "["
+echo -n "<fc=#96d870>"
 filled_length=$( echo "$percentage * $length/ 100" | bc)
 for ((i=0; i<$filled_length; i++))
 do
-  echo -n "#"
+  echo -n "█"
 done
 
+semifilled_box=$( echo "(2 * $percentage * $length/ 100) > (2*$filled_length) " | bc)
+if [[ $semifilled_box = 1 ]]; then
+  filled_length=$(($filled_length + 1))
+  echo -n "░"
+fi
+echo -n "</fc>"
+
+
+
+echo -n "<fc=#000000>"
 for ((i=$filled_length; i<$length; i++))
 do
-  echo -n " "
+  echo -n "█"
 done
+echo -n "</fc>"
 
 #Remove trailing and leading zeros
 percentage=$(echo $1 | sed '/\./ s/\.\{0,1\}0\{1,\}$//' | sed 's/^0*\([1-9]\)/\1/')
 #Print normalized to two significant decimals
-echo "] ($(echo "scale=2; $percentage / 1" | bc)%)"
-
-
-
-
+printf "](%3.${decimals}f%%)" "$(echo "scale=$decimals; $percentage / 1" | bc)"
 
--- xmobar.hs
+++ xmobar.hs
@@ -15,12 +15,12 @@ Config {
         Run MultiCpu ["-t","Cpu: <total0> <total1> <total2> <total3>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
         Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Com "/bin/bash" ["-c", "/home/giannis/.xmonad/progress.sh $(/home/giannis/.xmonad/volume.sh) 10"] "volume" 1,
+        Run Com "/bin/bash" ["-c", "/home/giannis/.xmonad/progress.sh $(/home/giannis/.xmonad/volume.sh) 10 0"] "volume" 1,
         Run Network "eno1" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run Date "%a %b %_d %l:%M" "date" 10,
         Run StdinReader
     ],
     sepChar = "%",
     alignSep = "}{",
-    template = "%StdinReader% }{ %volume%   %multicpu%   %memory%   %swap%   %em1%   <fc=#FFFFCC>%date%</fc>   %KPAO%"
+    template = "%StdinReader% }{ %multicpu%  Vol: %volume%  %memory%   %swap%   %em1%   <fc=#FFFFCC>%date%</fc>   %KPAO%"
 }
